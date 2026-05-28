53d1811469e6f7bb1e698e38b94f270d43429245
checull <gabrieltibichi062@gmail.com>
2026-05-29 00:46:03 +0300

    initial skeleton

---
+++ SimCase.cpp
@@ -0,0 +1,32 @@
+//libraries
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <ncurses.h>
+
+//data structures
+
+//global variables
+
+//function calls
+
+void init(); //initialization
+
+void RollAnimation(); //rolling animation (duh)
+
+void MainMenu(); //main menu selection and idk what
+
+void ItemList(); // item list where you can add or remove items
+
+
+int main()
+{
+    std::cout<<"hello world!";
+    return 0;
+}
+
+
+void init()
+{
+
+}
