fc1d71f748d24141715b09101c02cf0b15c74b41
checull <gabrieltibichi062@gmail.com>
2026-05-29 01:44:26 +0300

    Initialization Complete

--- SimCase.cpp
+++ SimCase.cpp
@@ -7,7 +7,7 @@
 //data structures
 
 //global variables
-
+int ScreenWidth, ScreenHeight;
 //function calls
 
 void init(); //initialization
@@ -21,12 +21,22 @@ void ItemList(); // item list where you can add or remove items
 
 int main()
 {
-    std::cout<<"hello world!";
+    init();
     return 0;
 }
 
 
 void init()
 {
-
+    char mesg[]="Just a string";
+    initscr();
+    //curs_set(0);
+    getmaxyx(stdscr, ScreenHeight, ScreenWidth);
+    noecho();
+    wborder(stdscr, '|', '|', '_', '_', '+', '+', '+', '+');
+    refresh();
+    mvprintw(ScreenHeight/2, (ScreenWidth-strlen("initialization complete."))/2, "Initialization complete.");
+    mvprintw(ScreenHeight/2+1, (ScreenWidth-strlen("Press any key to continue."))/2, "Press any key to continue.");
+    getch();
+    endwin();
 }
